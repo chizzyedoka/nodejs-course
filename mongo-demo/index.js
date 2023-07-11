147a44a707c4d960be706004092886e7ccf4158a
CHISOM EDOKA <edokachisom48@gmail.com>
2023-07-11 11:56:59 +0100

    built- in - validators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,8 +1,73 @@
 const mongoose = require("mongoose");
 // mongoose.set("strictQuery", false);
 
+mongoose
+  .connect("mongodb://0.0.0.0:27017/playground", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 5, maxlength: 255 }, // match: /pattern/ }, // validation
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "network"], // validation
+  },
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 200,
+  }, // validation
+});
+
+const Course = mongoose.model("Course", courseSchema); // creating a course class
+// model name, schema  // the model name is the singular version of the collection name
+
+// saving the object to the database
+async function createCourse() {
+  // creating an object based on the class
+  const course = new Course({
+    name: "my Course",
+    category: "-",
+    author: "chizzy",
+    tags: ["angular", "frontend"],
+    isPublished: true,
+    price: 15,
+  });
+  try {
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    console.log(ex.message);
+  }
+}
+
+createCourse();
+
+// // getting the courses from the database
+// async function getCourses() {
+//   const courses = await Course.find({ author: "Mosh", isPublished: true })
+//     .limit(10)
+//     .sort({ name: 1 })
+//     .select({ name: 1, tags: 1 });
+//   console.log(courses);
+// }
+// getCourses();
+
 // mongoose
-//   .connect("mongodb://0.0.0.0:27017/playground", {
+//   .connect("mongodb://0.0.0.0:27017/mongo-exercises", {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
 //     useFindAndModify: false,
@@ -15,58 +80,12 @@ const mongoose = require("mongoose");
 //   name: String,
 //   author: String,
 //   tags: [String],
-//   date: { type: Date, default: Date.now },
+//   date: Date,
 //   isPublished: Boolean,
+//   price: Number,
 // });
 
-// const Course = mongoose.model("Course", courseSchema); // creating a course class
-// // model name, schema  // the model name is the singular version of the collection name
-
-// // saving the object to the database
-// async function createCourse() {
-//   // creating an object based on the class
-//   const course = new Course({
-//     name: "Angular Course",
-//     author: "Mosh",
-//     tags: ["angular", "frontend"],
-//     isPublished: true,
-//   });
-//   const result = await course.save();
-//   console.log(result);
-// }
-
-// // createCourse();
-
-// // getting the courses from the database
-// async function getCourses() {
-//   const courses = await Course.find({ author: "Mosh", isPublished: true })
-//     .limit(10)
-//     .sort({ name: 1 })
-//     .select({ name: 1, tags: 1 });
-//   console.log(courses);
-// }
-// getCourses();
-
-mongoose
-  .connect("mongodb://0.0.0.0:27017/mongo-exercises", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...", err));
-
-const courseSchema = new mongoose.Schema({
-  name: String,
-  author: String,
-  tags: [String],
-  date: Date,
-  isPublished: Boolean,
-  price: Number,
-});
-
-const Course = mongoose.model("Course", courseSchema);
+// const Course = mongoose.model("Course", courseSchema);
 
 // async function specificCourses() {
 //   return await Course.find({
@@ -96,18 +115,18 @@ const Course = mongoose.model("Course", courseSchema);
 // findById()
 // modify its properties
 // save
-async function updateCourse() {
-  const course = await Course.find({ _id: "5a68fde3f09ad7646ddec17e" });
-  if (!course) {
-    console.log("not available");
-    return;
-  }
-  course.isPublished = true;
-  course.author = "Another Author";
-  const result = await course.save();
-  console.log(result);
-}
-updateCourse();
+// async function updateCourse(id) {
+//   const course = await Course.findById(id);
+//   if (!course) {
+//     console.log("not available");
+//     return;
+//   }
+//   course.isPublished = true;
+//   course.author = "Another Author";
+//   const result = await course.save();
+//   console.log(result);
+// }
+// updateCourse();
 
 //update first approach
 // update directly
